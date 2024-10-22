76e03c82eef07b36dd132a9bf93b914d61f1b79e
Yagnik <yagnikhingrajiya56@gmail.com>
2024-10-22 19:26:59 +0530

    fix gitignore

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCETzkKRNy5mPilxFoxKlQBG8wgTDx95Wg",
-  authDomain: "moviemania-5f1c1.firebaseapp.com",
-  projectId: "moviemania-5f1c1",
-  storageBucket: "moviemania-5f1c1.appspot.com",
-  messagingSenderId: "917672510159",
-  appId: "1:917672510159:web:dabffd9d9ab485e6cc6dba",
-  measurementId: "G-B57NJDT6BM"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
